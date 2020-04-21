ba9b32e75dc220e9428575f7ba2ceb85a47ded2f
Tiago Deliberali <tiago.deliberali@gmail.com>
2020-04-21 20:02:50 -0300

    added draw

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ pub struct Chip8 {
     stack_position: usize,
     i: usize,
     registers: [u8; 16],
+    screen: [[u8; 64]; 32],
 }
 
 impl Chip8 {
@@ -34,6 +35,7 @@ impl Chip8 {
             stack_position: 0,
             i: 0,
             registers: [0; 16],
+            screen: [[0; 64]; 32],
         }
     }
 
@@ -56,21 +58,30 @@ impl Chip8 {
 
         match instruction {
             0x0000 => return false,
+            0x00E0 => self.clear_screen(),
+            0x3000..=0x3FFF => self.skip_next_position(self.registers[register_x] == mask),
+            0x4000..=0x4FFF => self.skip_next_position(self.registers[register_x] != mask),
             0xA000..=0xAFFF => self.i = address,
             0xC000..=0xCFFF => self.set_random_value(register_x, mask),
+            0xD000..=0xDFFF => self.draw(register_x, register_y, sub_op),
             _ => println!("UNKNOWN OPCODE: {:04x}", instruction),
         }
 
         self.print_state();
+        self.print_screen();
         self.memory_position += 2;
 
         return true;
     }
 
+    fn skip_next_position(&mut self, should_skip: bool) {
+        if should_skip {
+            self.memory_position += 2;
+        }
+    }
+
     fn set_random_value(&mut self, register_id: usize, mask: u8) {
-        let x: u8 = random();
-        debug(format!("RANDOM NUMBER: {:08b}", x));
-        debug(format!("MASK: {:08b}", mask));
+        let x: u8 = 0_u8; // random();
         self.registers[register_id] = x & mask;
     }
 
@@ -78,6 +89,43 @@ impl Chip8 {
         let instruction: u16 = self.memory[self.memory_position] as u16;
         let instruction_2: u16 = self.memory[self.memory_position + 1] as u16;
         instruction << 8 | instruction_2
+    }
+
+    fn clear_screen(&mut self) {
+        self.screen = [[0; 64]; 32];
+    }
+
+    fn draw(&mut self, position_x: usize, position_y: usize, bytes: usize) {
+        for i in 0..bytes {
+            let data = self.memory[self.i + i];
+            debug(format!("{:08b}", data));
+
+            self.screen[position_x + 7][position_y + i] = (data as u8) & 0b0000_0001;
+            self.screen[position_x + 6][position_y + i] = ((data as u8) & 0b0000_0010) >> 1;
+            self.screen[position_x + 5][position_y + i] = ((data as u8) & 0b0000_0100) >> 2;
+            self.screen[position_x + 4][position_y + i] = ((data as u8) & 0b0000_1000) >> 3;
+
+            self.screen[position_x + 3][position_y + i] = ((data as u8) & 0b0001_0000) >> 4;
+            self.screen[position_x + 2][position_y + i] = ((data as u8) & 0b0010_0000) >> 5;
+            self.screen[position_x + 1][position_y + i] = ((data as u8) & 0b0100_0000) >> 6;
+            self.screen[position_x + 0][position_y + i] = ((data as u8) & 0b1000_0000) >> 7;
+        }
+    }
+
+    fn print_screen(&self) {
+        println!("╔════════════════════════════════════════════════════════════════╗");
+        for i in self.screen.iter() {
+            print!("║");
+            for j in i.iter() {
+                if *j == 0_u8 {
+                    print!("░");
+                } else {
+                    print!("█");
+                }
+            }
+            println!("║");
+        }
+        println!("╚════════════════════════════════════════════════════════════════╝");
     }
 
     fn print_state(&self) {
@@ -142,5 +190,53 @@ mod tests {
 
         assert_eq!(should_iterate, true);
         assert_eq!(chip.registers[0x02], 0x00);
+    }
+
+    #[test]
+    fn should_skip_if_equals_on_3xxx() {
+        let program = vec![0x32, 0x01];
+        let mut chip = Chip8::new_with_memory(program);
+        chip.registers[0x02] = 0x01;
+
+        let should_iterate = chip.single_iteration();
+
+        assert_eq!(should_iterate, true);
+        assert_eq!(chip.memory_position, 0x200 + 4);
+    }
+
+    #[test]
+    fn should_not_skip_if_different_on_3xxx() {
+        let program = vec![0x32, 0x01];
+        let mut chip = Chip8::new_with_memory(program);
+        chip.registers[0x02] = 0x02;
+
+        let should_iterate = chip.single_iteration();
+
+        assert_eq!(should_iterate, true);
+        assert_eq!(chip.memory_position, 0x200 + 2);
+    }
+
+    #[test]
+    fn should_not_skip_if_equals_on_4xxx() {
+        let program = vec![0x42, 0x01];
+        let mut chip = Chip8::new_with_memory(program);
+        chip.registers[0x02] = 0x01;
+
+        let should_iterate = chip.single_iteration();
+
+        assert_eq!(should_iterate, true);
+        assert_eq!(chip.memory_position, 0x200 + 2);
+    }
+
+    #[test]
+    fn should_skip_if_different_on_4xxx() {
+        let program = vec![0x42, 0x01];
+        let mut chip = Chip8::new_with_memory(program);
+        chip.registers[0x02] = 0x02;
+
+        let should_iterate = chip.single_iteration();
+
+        assert_eq!(should_iterate, true);
+        assert_eq!(chip.memory_position, 0x200 + 4);
     }
 }
